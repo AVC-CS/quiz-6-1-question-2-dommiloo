e9a138a3374c4d4ecbd837d182943a068b9c486c
dommiloo <156959975+dommiloo@users.noreply.github.com>
2024-03-16 01:33:33 +0000

    1st commit

--- main.hpp
+++ main.hpp
@@ -1,3 +1,25 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int writeFile(string& filename)
+{
+    ofstream ofs(filename);
+    if (!ofs) {
+        cerr << "Error " << erndl; 
+        return 0;
+    }
+
+    int numEmpl;
+    cout << "Enter number of employees: " 
+    cin >> numEmpl;
+
+    ofs << numEmpl << endl;
+
+    for (int i = 0;i < numEmpl; ++i) {
+        int id;
+        strin
+    }
+}
